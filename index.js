5375974ce3bc5315b41afd2a1bfd6a07df7cce49
cskati <csefi.katalin@gmail.com>
2023-03-31 11:44:23 +0200

    magyarázatok

--- index.js
+++ index.js
@@ -1,82 +1,90 @@
-const cell = document.querySelectorAll(".cell");
+const cells = document.querySelectorAll(".cell");  //megfogja az összes .cells divet a HTML-ben
 const statusText = document.querySelector("#statusText");
 const restartBtn = document.querySelector("#restartBtn");
 const winConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
 ];
-let options = ["", "", "", "", "", "", "", "", ""];
+const options = ["", "", "", "", "", "", "", "", ""];
+
 let currentPlayer = "X";
 let running = false;
 
 initializeGame();
 
-function initializeGame(){
-    cells.forEach(cell => cell.addEventListener("click",cellClicked))
-    restartBtn.addEventListener("click", restartGame);
-    statusText.textContent = `${currentPlayer}'s turn`;
-    running = true;
-}
-function cellClicked(){
-    const cellIndex = this.getAttribute("cellIndex");
+function initializeGame() {
 
-    if(options[cellIndex] != "" || !running){
-        return;
+  console.log(cells);
+  for (let index = 0; index < cells.length; index++) {
+    cells[index].addEventListener("click", cellClicked); //rátesszük az összes elemre a kattintás eseménykezelőt
+  }
+  // cells.forEach(cell => cell.addEventListener("click",cellClicked))
+  restartBtn.addEventListener("click", restartGame);
+  statusText.textContent = `${currentPlayer}'s turn`;
+  running = true;
+}
+function cellClicked() {
+  //ez hívódik akkor, amikor rákattintottunk a div-re
+  const cellIndex = this.getAttribute("cellIndex"); //html elem attributumának lekérdezése
+
+  if (options[cellIndex] != "" || !running) {
+    return;
+  }
+
+  updateCell(this, cellIndex);
+  checkWinner();
+}
+function updateCell(cell, index) {
+  options[index] = currentPlayer;
+  cell.textContent = currentPlayer; //ígyy írunk tartalmat a html-be
+}
+function changePlayer() {
+  currentPlayer = currentPlayer == "X" ? "O" : "X";
+  statusText.textContent = `${currentPlayer}'s turn`;
+}
+function checkWinner() {
+  let roundWon = false;
+
+  for (let i = 0; i < winConditions.length; i++) {
+    const condition = winConditions[i];
+    const cellA = options[condition[0]];
+    const cellB = options[condition[1]];
+    const cellC = options[condition[2]];
+
+    if (cellA == "" || cellB == "" || cellC == "") {
+      continue;
     }
-
-    updateCell(this, cellIndex);
-    checkWinner();
-}
-function updateCell(cell, index){
-    options[index] = currentPlayer;
-    cell.textContent = currentPlayer;
-}
-function changePlayer(){
-    currentPlayer = (currentPlayer == "X") ? "O" : "X";
-    statusText.textContent = `${currentPlayer}'s turn`;
-}
-function checkWinner(){
-    let roundWon = false;
-
-    for(let i = 0; i < winConditions.length; i++){
-        const condition = winConditions[i];
-        const cellA = options[condition[0]];
-        const cellB = options[condition[1]];
-        const cellC = options[condition[2]];
-    
-        if(cellA == "" || cellB == "" || cellC == ""){
-            continue;
-        }
-        if(cellA == cellB && cellB == cellC){
-            roundWon = true;
-            break;
-        }
+    if (cellA == cellB && cellB == cellC) {
+      roundWon = true;
+      break;
     }
+  }
 
-    if (roundWon){
-        statusText.textContent = `${currentPLayer} wins!`;
-        running = false;
-    }
-    else if(!options.includes("")){
-        statusText.textContent = `Draw!`;
-        running = false;
-
-    }
-    else{
-        changePlayer();
-    }
+  if (roundWon) {
+    statusText.textContent = `${currentPLayer} wins!`;
+    running = false;
+  } else if (!options.includes("")) {
+    statusText.textContent = `Draw!`;
+    running = false;
+  } else {
+    changePlayer();
+  }
 }
-function restartGame(){
-    currentPlayer = "X";
-    let options = ["", "", "", "", "", "", "", "", ""];
-    statusText.textContent = `${currentPlayer}'s turn`;
-    cells.forEach(cell => cell.textContent = "");
-    running = true;
-
+function restartGame() {
+  currentPlayer = "X";
+ 
+  let options = ["", "", "", "", "", "", "", "", ""];
+  statusText.textContent = `${currentPlayer}'s turn`;
+  cells.forEach((cell) => (cell.textContent = ""));
+  running = true;
 }
+
+
+// feladat:  jeleníts meg képeket egy oldalon div-ekben. A képekre kattintva a képek elérési útja kerüljön bele egy listába. 
+/**A kiválasztott kép jelenjen meg a sok kép fölött egy külön section tárolóban */
